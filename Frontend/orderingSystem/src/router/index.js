256711f294cda051102ceee0c984bddd7711fc96
Kuang Jiaming <146501876+kuangjm123@users.noreply.github.com>
2025-07-02 14:10:50 +0800

    111

--- Frontend/orderingSystem/src/router/index.js
+++ Frontend/orderingSystem/src/router/index.js
@@ -1,12 +1,18 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import login from './components/login.vue';//引入About
+import login from './components/login.vue';
+import userHome from '../view/userView/userHome.vue';
+import component from 'element-plus/es/components/tree-select/src/tree-select-option.mjs';
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: '/login', 
-  component: login }
+  component: login },
+  {
+    path:'/userHome',
+    component:userHome
+  }
 ];
 
 const router = new VueRouter({
---
+++ Frontend/orderingSystem/src/view/userView/userHome.vue
@@ -0,0 +1,12 @@
+<!-- 用户主页面 -->
+<template>
+    
+</template>
+  
+<script>
+  
+  export default {
+    name: "LoginForm",
+  };
+</script>
+  
